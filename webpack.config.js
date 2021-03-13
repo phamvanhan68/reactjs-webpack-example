6f0c7d008f01a0cf2e6d86f846dcfbc534e890df
Han (Hanson) V. PHAM <hapham@philly.com>
2021-03-13 15:04:32 +0700

    fix-bug(webpack): hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+let mode = 'development';
+let target = 'web';
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = "browserslist";
+}
+
 module.exports = {
-mode: 'development',
+  mode: mode,
+  target:target,
   entry: "./src/index.js",
   output: {
-    filename: "bundle.[hash].js",
+    filename: "[name].[fullhash].js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new MiniCssExtractPlugin(),
+    
   ],
   resolve: {
     modules: [__dirname, "src", "node_modules"],
@@ -26,7 +38,12 @@ mode: 'development',
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ],
       },
       {
         test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$|\.wav$|\.mp3$|\.ico$/,
